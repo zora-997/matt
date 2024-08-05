217b415fcb2aa83dca593d73c4134b04817f7a9c
unknown <zorabrostam315@gmail.com>
2024-08-05 17:26:38 +0300

    danani teeth class u pdate krdni

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import  { useContext } from 'react';
 import { Footer } from './layouts/Footer';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './Routes/appRoutes';
-import Loader from './components/Loader';
-import { useState, useEffect } from 'react';
+// import Loader from './components/Loader';
+// import { useState, useEffect } from 'react';
 
 
 
@@ -14,25 +14,25 @@ import { useState, useEffect } from 'react';
 function App() {
   const { language } = useContext(LanguageContext);
 
-  // loading page
-  const [isLoading, setIsLoading] = useState(true);
+  // // loading page
+  // const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        // Simulate data fetching
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 3000); // Replace with your actual data fetching logic
-    }, []);
+  //   useEffect(() => {
+  //       // Simulate data fetching
+  //       setTimeout(() => {
+  //           setIsLoading(false);
+  //       }, 3000); // Replace with your actual data fetching logic
+  //   }, []);
 
   return (
     <div className={`    ${language!=='en'?'font-arfont':''}`} dir={`${language === 'en'?'ltr':'rtl'}`}>
-       {isLoading ? <Loader /> : (
+      
                 <BrowserRouter>
                     <Navbar />
                     <AppRoutes />
                     <Footer />
                 </BrowserRouter>
-            )}
+          
     </div>
   );
 }
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -4,6 +4,7 @@ import React, {
   useRef,
   useCallback,
   useMemo,
+  useState
 } from "react";
 import { LanguageContext } from "../Context/LanguageContext";
 
@@ -23,6 +24,7 @@ import bg_7 from "../assets/BG/BG7.jpg";
 // import bg_9 from "../assets/BG/IMG_9876.jpg";
 
 import "aos/dist/aos.css";
+import Loader from "../components/Loader";
 const Hero = () => {
   const { texts } = useContext(LanguageContext);
   const imageRef = useRef(null);
@@ -31,13 +33,15 @@ const Hero = () => {
   const characters = text.split("");
   const textLine = "-----------------------------------------------------";
   const charactersLine = textLine.split("");
+  const [isLoading, setIsLoading] = useState(true);
 
   const images = useMemo(
     //  bg_6, bg_7, bg_8, bg_9
-    () => [bg_1,bg_5,bg_7,bg_2, bg_4, bg_3,bg_6,],
+    () => [bg_1,bg_5,bg_7,bg_2, bg_4, bg_3,bg_6],
     []
   );
   // const textReveal = () => {
+
   //   return(<h1 className="text-3xl font-bold text-white">
   //   {characters.map((char, index) => (
   //     <span
@@ -52,6 +56,9 @@ const Hero = () => {
   //   ))}
   // </h1>)
   // }
+    // loading page
+   
+
   const imagesMemo = useMemo(() => images, [images]);
   const nextImage = useCallback(() => {
     currentIndexRef.current = (currentIndexRef.current + 1) % imagesMemo.length;
@@ -68,75 +75,88 @@ const Hero = () => {
     };
   }, [nextImage]);
 
+
+
+    useEffect(() => {
+      if (bg_1.length && bg_5.length   ) {
+        setIsLoading(false)
+      }
+    }, []);
+
   return (
-    <div className="w-full   h-[100%]  ">
-      <img
-        className="w-screen  h-screen object-cover brightness-50 "
-        ref={imageRef}
-        src={images[0]}
-        alt="Slideshow"
-      />
-    
-
-      <div className="absolute  top-0 left-0  bottom-0 right-0 text-start z-10 px-2 flex flex-col justify-center items-center  ">
-        {/* text title hero */}
-        {/* <h1
-          className="text-white font-bold text-5xl max-w-3xl  "
-          data-aos="fade-right"
-          data-aos-offset="200"
-          data-aos-easing="ease-in-sine"
-          data-aos-duration="600"
-          
-          
-        >
-          {texts.hero_title}
-        </h1> */}
-
-        <h1 className="text-white  text-2xl max-w-md sm:text-4xl sm:max-w-3xl ">
-          {charactersLine.map((char, index) => (
-            <span
-              key={index}
-              className={`inline-block ${
-                index === 0 ? "" : "animate-text-reveal"
-              }`}
-              style={{ animationDelay: `${index * 0.1}s` }}
-            >
-              {char}
-            </span>
-          ))}
-        </h1>
-
-        {/* text title hero */}
-        <h1 className="text-white font-bold border-l-2 border-t-2 p-2 text-2xl max-w-md sm:text-5xl sm:max-w-3xl 2xl:text-7xl 2xl:max-w-6xl ">
-          {characters.map((char, index) => (
-            <span
-              key={index}
-              className={`inline-block ${
-                index === 0 || char === ""  ? "" : "animate-text-reveal pr-1"
-              }`}
-              style={{ animationDelay: `${index * 0.1}s` }}
-            >
-              {char}
-            </span>
-          ))}
-        </h1>
-
-        <h1
-          className="text-white text-sm md:text-lg max-w-md sm:max-w-3xl  p-2  mt-5"
-          data-aos="fade-down"
-        >
-          {texts.hero_subtitle}
-        </h1>
-        {/* <a
-          href="about"
-          className="mt-14 bg-[#307fc0] flex justify-center items-center text-black font-bold rounded w-56 h-12 hover:bg-gray-700 hover:text-white hover:border-white hover:border"
-          data-aos="fade-up"
-        >
-          {texts.about_us}
-        </a> */}
-     
+<>
+    {isLoading ? <Loader /> : (
+      <div className="w-full   h-[100%]  ">
+        <img
+          className="w-screen  h-screen object-cover brightness-50 "
+          ref={imageRef}
+          src={images[0]}
+          alt="Slideshow"
+        />
+      
+  
+        <div className="absolute  top-0 left-0  bottom-0 right-0 text-start z-10 px-2 flex flex-col justify-center items-center  ">
+          {/* text title hero */}
+          {/* <h1
+            className="text-white font-bold text-5xl max-w-3xl  "
+            data-aos="fade-right"
+            data-aos-offset="200"
+            data-aos-easing="ease-in-sine"
+            data-aos-duration="600"
+            
+            
+          >
+            {texts.hero_title}
+          </h1> */}
+  
+          <h1 className="text-white  text-2xl max-w-md sm:text-4xl sm:max-w-3xl ">
+            {charactersLine.map((char, index) => (
+              <span
+                key={index}
+                className={`inline-block ${
+                  index === 0 ? "" : "animate-text-reveal"
+                }`}
+                style={{ animationDelay: `${index * 0.1}s` }}
+              >
+                {char}
+              </span>
+            ))}
+          </h1>
+  
+          {/* text title hero */}
+          <h1 className="text-white font-bold border-l-2 border-t-2 p-2 text-2xl max-w-md sm:text-5xl sm:max-w-3xl 2xl:text-7xl 2xl:max-w-6xl ">
+            {characters.map((char, index) => (
+              <span
+                key={index}
+                className={`inline-block ${
+                  index === 0 || char === ""  ? "" : "animate-text-reveal pr-1"
+                }`}
+                style={{ animationDelay: `${index * 0.1}s` }}
+              >
+                {char}
+              </span>
+            ))}
+          </h1>
+  
+          <h1
+            className="text-white text-sm md:text-lg max-w-md sm:max-w-3xl  p-2  mt-5"
+            data-aos="fade-down"
+          >
+            {texts.hero_subtitle}
+          </h1>
+          {/* <a
+            href="about"
+            className="mt-14 bg-[#307fc0] flex justify-center items-center text-black font-bold rounded w-56 h-12 hover:bg-gray-700 hover:text-white hover:border-white hover:border"
+            data-aos="fade-up"
+          >
+            {texts.about_us}
+          </a> */}
+       
+        </div>
       </div>
-    </div>
+     
+  )}
+</>
   );
 };
 
