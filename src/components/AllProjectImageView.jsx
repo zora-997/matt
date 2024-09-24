d279a068855bbb125c264088e92e5cce9499da54
zora-997 <zorabrostam315@gmail.com>
2024-09-24 18:00:38 +0300

    show image in mobile of the allproject

--- src/components/AllProjectImageView.jsx
+++ src/components/AllProjectImageView.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const AllProjectImageView = ({ isVisible, onClose, image }) => {
   if (!isVisible) return null;
   return (
-    <div onClick={()=> onClose(false)} className="hidden  fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm   lg:flex justify-center   items-start z-50">
-      <div className="w-[800px] h-[300px] mt-20   ">
-        <img src={image} alt="show" className="w-[1000px] h-[800px]  " />
-      </div>
+    <div onClick={()=> onClose(false)} className=" fixed inset-0  bg-black bg-opacity-25 md:backdrop-blur-md backdrop-blur-sm   flex justify-center   items-center z-50">
+      
+        <img src={image} alt="show" className="lg:w-[1000px] lg:h-[800px]    object-cover  lg:px-5 " />
+     
     </div>
   );
 };
